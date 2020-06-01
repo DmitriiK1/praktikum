1aefc8a77a601d5ac4530116f911accc2ee6c408
DmitriiK1 <dimonovec@mail.ru>
2020-06-01 15:58:52 +0300

    Настройка webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,77 @@
-// // webpack.config.js
-// const path = require('path');
-// // подключаем path к конфигу вебпак
-
-// module.exports = {
-//     entry: { main: './src/scripts/script.js' },
-//     output: {
-//         path: path.resolve(__dirname, 'dist'),
-//         filename: 'main.js'
-//     }
-// }
 // переписали точку выхода, используя утилиту path
 const path = require('path');
+const webpack = require('webpack');
+// webpack.config.js
+
+new webpack.DefinePlugin({
+    'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+})
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// подключаем плагин
+const isDev = process.env.NODE_ENV === 'development';
+// создаем переменную для development-сборки
 
 module.exports = {
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
         port: 3000
-      },
+    },
     entry: { main: './src/scripts/script.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
+    },
+    module: {
+        rules: [{ // тут описываются правила
+            test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+            use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
+            exclude: /node_modules/ // исключает папку node_modules
         },
-module: {
-    rules: [{ // тут описываются правила
-        test: /\.js$/, // регулярное выражение, которое ищет все js файлы
-        use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
-        exclude: /node_modules/ // исключает папку node_modules
-            }
-        ]
-    }
+        // пример настройки плагина image-webpack-loader
+        {
+            test: /\.html$/,
+            loader: 'html-loader'
+        },
+        {
+            test: /\.(png|jpg|gif|ico|svg)$/,
+            use: [
+                'file-loader?name=../dist/images/[name].[ext]', // указали папку, куда складывать изображения
+                {
+                    loader: 'image-webpack-loader',
+                    options: {}
+                },
+            ]
+        },
+        {
+            test: /\.css$/i,
+            use: [
+                (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+                'css-loader',
+                'postcss-loader'
+            ]
+
+        },
+        {
+            test: /\.(eot|ttf|woff|woff2)$/,
+            loader: 'file-loader?name=./vendor/[name].[ext]'
+        }
+        ],
+    },
+plugins: [
+    new MiniCssExtractPlugin({
+        filename: 'style.[contenthash].css'
+    }),
+    new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /\.css$/g,
+        cssProcessor: require('cssnano'),
+        cssProcessorPluginOptions: {
+            preset: ['default'],
+        },
+        canPrint: true
+    }),
+    new HtmlWebpackPlugin()
+]
 };
